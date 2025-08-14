f4512ffb0d4c28f4dad15e8f16abae35d80429d5
Mohammed <GM+mami186@users.noreply.github.com>
2025-08-14 16:39:54 +0300

    refactor commit context

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     <nav className="navbar d-flex justify-content-between">
       <div>User:{user}</div>
       <span className="badge text-bg-secondary">{tasks.length}</span>
-      <LoginStatus />
+      <LoginStatus />  
     </nav>
   );
 };
